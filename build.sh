9e8aa9b3c1f2e784efa5b19a82104192b246d097
Grarak <williye97@gmail.com>
2013-08-30 15:04:39 +0200

    fancykernel ramdisk
    Signed-off-by: Grarak <williye97@gmail.com>

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+rm -rf kernel.zip
+rm -rf ramdisk.gz
+find -name "*~" -exec rm -rf {} \;
+
+build () {
+    cd boot.img-ramdisk
+    find . | cpio -o -H newc | gzip -9 > ../ramdisk.gz
+    cd ..
+    ./mkbootimg-$1 --kernel zImage --ramdisk ramdisk.gz --base 0x80000000 -o boot.img
+}
+
+if grep -q "export BUILD_MAC_SDK_EXPERIMENTAL=1" ~/.bash_profile; then
+    build darwin
+else
+    build linux
+fi
+
+mv -v boot.img out/
+cd out
+adb push boot.img /sdcard/romswitcher/second.img
+zip -r kernel.zip META-INF boot.img
+mv -v kernel.zip ../
+cd ..
+adb push kernel.zip /sdcard/0/
